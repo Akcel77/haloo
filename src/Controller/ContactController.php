f22eb6eb76b5ca00ddb30acdcb3d7acd1b5493b9
Akcel77 <Akeucel@gmail.com>
2022-10-08 12:49:27 +0200

    Contact controller

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function index(Request $request): Response
+    {
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('notice', 'Votre message a bien été envoyé. Notre équipe vous répondra dans les plus brefs délais.');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
