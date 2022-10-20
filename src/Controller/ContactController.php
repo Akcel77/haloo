fb31f7f1067b7b3b48cfe064102fa7b00376f8c8
Akcel77 <Akeucel@gmail.com>
2022-10-20 12:46:45 +0200

    Send mail import and clear form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,7 +19,14 @@ class ContactController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $mail = new Mail();
+            $content = "Vous avez reçu un message de <strong>".$form->getData()['prenom']." ".$form->getData()['nom']."</strong></br><br>Adresse email : <strong>".$form->getData()['email']."</strong> </br><br><strong>Message : </strong><br>".$form->getData()['content']."</br></br>";
+
+            $mail->send('contact@looha.io', 'Contact Looha', 'Vous avez recu une nouvelle demande de contact', $content);
+
             $this->addFlash('notice', 'Votre message a bien été envoyé. Notre équipe vous répondra dans les plus brefs délais.');
+            return $this->redirect($request->getUri());
+
         }
 
         return $this->render('contact/index.html.twig', [
